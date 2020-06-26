8b67c92683e5c9ddab55d40ab74d9a32b2315051
Nicholas Davies <nicholasjohndavies42@gmail.com>
2020-06-26 19:24:22 +0800

    file restructuring for heroku

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -5,6 +5,9 @@ const path = require("path");
 module.exports = function(app) {
 
     // GET route for getting all workouts
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
     app.get("/exercise", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
